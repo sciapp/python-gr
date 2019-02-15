322d63e396c4ecd5608fa47470a953aad8960cfd
Daniel Kaiser <d.kaiser@fz-juelich.de>
2019-02-15 09:54:32 +0100

    Added volume function to GR wrapper and mlab, increased the required GR runtime version to 0.38.0, minor fixes

--- gr/runtime_helper.py
+++ gr/runtime_helper.py
@@ -10,7 +10,7 @@ import sys
 
 def required_runtime_version():
     # TODO: load runtime version from file
-    return '0.37.0'
+    return '0.38.0'
 
 
 def _version_string_to_tuple(version_string):
