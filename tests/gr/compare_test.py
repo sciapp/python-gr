f2aca3e7bb00e079b5f811483b0aba016b1c95ae
Maximilian Heuwes <m.heuwes@fz-juelich.de>
2019-12-10 14:09:12 +0100

    Making tests compatible with gr-test v0.1.0

--- tests/gr/compare_test.py
+++ tests/gr/compare_test.py
@@ -9,6 +9,7 @@ from nose import with_setup
 from gr_test import CompareResult
 from gr_test import python_image as image_data
 from gr_test import python_video as video_data
+from gr_test.entry_points import safe_mkdir
 base_path = os.path.abspath(os.path.dirname(os.path.realpath(__name__)) + '/../../test_result/')
 
 if 'GR_TEST_BASE_PATH' in os.environ:
@@ -33,8 +34,8 @@ def test_images():
     consistency, pairs = image_data.get_test_data()
     for x in consistency:
         yield succeed_if_none, x
-    for dir, ext, ref_name, test_name in pairs:
-        yield compare, dir, ext, ref_name, test_name
+    for dir, ext, ref_name, test_name, base_name in pairs:
+        yield compare, dir, ext, ref_name, test_name, base_name
 
 @with_setup(setup_func)
 def test_video():
@@ -42,20 +43,28 @@ def test_video():
     consistency, pairs = video_data.get_test_data()
     for x in consistency:
         yield succeed_if_none, x
-    for dir, ext, ref_name, test_name in pairs:
-        yield compare, dir, ext, ref_name, test_name
+    for dir, ext, ref_name, test_name, base_name in pairs:
+        yield compare, dir, ext, ref_name, test_name, base_name
 
 def succeed_if_none(x):
     assert x is None
 
-def compare(dir, ext, ref_name, test_name):
+def compare(dir, ext, ref_name, test_name, base_name):
+    this_path = os.path.join(results_path, dir)
+    file_name = os.path.basename(test_name) # f.e. REFERENCE.pdf.png or frame-1.mov.png
+
     result = CompareResult(ref_name, test_name)
 
-    file_name = os.path.basename(ref_name)
     if not result.is_equal():
-        out_name = '%s/%s_%s_diff.png' % (results_path, dir, file_name)
+        safe_mkdir(this_path)
+
+        out_name = '%s/%s_diff.png' % (this_path, file_name)
         result.make_diff_png(out_name)
-        shutil.copy(test_name, '%s/%s.%s' % (results_path, dir, file_name))
+
+        # Copy generated files to output directory
+        shutil.copy(test_name, '%s/%s' % (this_path, file_name))
+        if base_name is not None:
+            shutil.copy(base_name, '%s/%s' % (this_path, os.path.basename(base_name)))
         print("diff png: %s" % out_name)
 
     assert result.is_equal()
